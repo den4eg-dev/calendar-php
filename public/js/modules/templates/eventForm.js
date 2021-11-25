3b2ccbd5e6076b9647c93e144d5333d38cddce88
Denis Kazakov <den4eg.dev@gmail.com>
2021-11-25 13:51:55 +0100

    types window added function

--- public/js/modules/templates/eventForm.js
+++ public/js/modules/templates/eventForm.js
@@ -13,8 +13,15 @@ export const eventForm = async (props, create = false) => {
 
     let currentType = types[0]
     let typesOutput = ``
+
+
     types.forEach(type => {
-        typesOutput += `<div id-data="${type.type_pk}" class="item ${type.type_name}">${type.type_name}</div> \n`
+        /**
+         * @param type          object of types.
+         * @param data.type_pk   primary key.
+         * @param data.type_name   name of type work/family/private.
+         */
+        typesOutput += `<div data-id="${type.type_pk}" class="item ${type.type_name}">${type.type_name}</div> \n`
     })
 
     const {event_pk, event_title, event_date, time_start, time_end, description, type_color, type_fk} = props
