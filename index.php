eaf9bf3e21933a015fabf624e1a447e9400267dc
Denis Kazakov <den4eg.dev@gmail.com>
2022-01-04 20:00:04 +0200

    composer added2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require __DIR__ . "/vendor/autoload.php";
 require __DIR__ . "/config.php";
 require_once __DIR__ . "/lib/functions/helpers.php";
 require __DIR__ . "/route.php";
-
+//sss
 
 // Create connection
 //$conn = new mysqli(DB_HOST, DB_LOGIN, DB_PASSWORD);
